8bdb9250fe1a7b0f506a9702d9019705d011c457
ioanna <psyllost@gmail.com>
2015-06-09 15:02:38 +0200

    Calls to /OCA/Activity instead of theme

--- ajax/getNew.php
+++ ajax/getNew.php
@@ -4,25 +4,22 @@
 \OCP\JSON::checkAppEnabled('activity');
 \OCP\JSON::checkAppEnabled('user_notification');
 
-OC::$CLASSPATH['Data']    ='/usr/local/www/owncloud/themes/deic_theme_oc7/apps/activity/lib/data.php';
-OC::$CLASSPATH['DataHelper'] = '/usr/local/www/owncloud/themes/deic_theme_oc7/apps/activity/lib/datahelper.php';
-OC::$CLASSPATH['ParameterHelper'] = '/usr/local/www/owncloud/themes/deic_theme_oc7/apps/activity/lib/parameterhelper.php';
-OC::$CLASSPATH['UserSettings'] = '/usr/local/www/owncloud/themes/deic_theme_oc7/apps/activity/lib/usersettings.php';
+OC::$CLASSPATH['Data']    = OC::$SERVERROOT.'/apps/user_notification/lib/data.php';
 
 $l = \OCP\Util::getL10N('activity');
 $data = new Data(\OC::$server->getActivityManager());
 
 $groupHelper = new \OCA\Activity\GroupHelper(
   \OC::$server->getActivityManager(),
-  new DataHelper(
+  new \OCA\Activity\DataHelper(
 	\OC::$server->getActivityManager(),
-	new ParameterHelper(new \OC\Files\View(''), $l),
+	new \OCA\Activity\ParameterHelper(new \OC\Files\View(''), $l),
 	$l
   ),
   true
 );
 
-$userSettings = new UserSettings(
+$userSettings = new \OCA\Activity\UserSettings(
 	\OC::$server->getActivityManager(),
 	new \OCP\IConfig,
 	$data);
