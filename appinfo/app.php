5b87b1ca927e7869eba3f9334879ee4519237f7f
deltafunction <frederik@orellana.dk>
2016-03-17 12:56:56 +0100

    Only notify on files on home server.

--- appinfo/app.php
+++ appinfo/app.php
@@ -2,7 +2,12 @@
 
 OCP\App::checkAppEnabled('user_notification');
   
-if(\OCP\User::isLoggedIn() ){
+if(\OCP\User::isLoggedIn() && strpos($_SERVER['REQUEST_URI'], '/index.php/settings')===FALSE &&
+	strpos($_SERVER['REQUEST_URI'], 'logout')===FALSE &&
+	strpos($_SERVER['REQUEST_URI'], '/ajax/')===FALSE &&
+	strpos($_SERVER['REQUEST_URI'], '/jqueryFileTree.php')===FALSE &&
+	strpos($_SERVER['REQUEST_URI'], '/firstrunwizard/')===FALSE &&
+	strpos($_SERVER['REQUEST_URI'], '/ws/')===FALSE){
   OCP\Util::addStyle('user_notification', 'notifications');
   OCP\Util::addScript('user_notification', 'notifications');
   OCP\Util::addScript('user_group_admin', 'user_group_notification'); 
