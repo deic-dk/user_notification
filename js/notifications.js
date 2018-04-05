f8d5af1bafc57df1c69bc9d3bbfd8a011867e8db
deltafunction <frederik@orellana.dk>
2018-04-06 00:09:49 +0200

    Localization

--- js/notifications.js
+++ js/notifications.js
@@ -225,7 +225,7 @@ function addActivityRow(){
 	row.children('a').attr('href', OC.generateUrl('/apps/activity'));
 	row.find('.row').children().remove();
 	row.find('.row').append('<div class="col-sm-11 col-sm-offset-1 col-xs-10 col-sx-offset-2"><div class="text-dark-gray"><i class="icon-flash deic_green icon"></i>'+
-			t('user_notification', 'All Activities')+'</div></div>');
+			t('user_notification', 'All activities')+'</div></div>');
 	row.removeClass('hidden');
 	$('li.notifications').children('ul').append(row);
 }
